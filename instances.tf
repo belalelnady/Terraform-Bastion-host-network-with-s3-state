bee453791eeb63ac0413c75f377317f577db79a6
belalelnady <belalelnadyz@gmail.com>
2024-09-04 20:56:13 +0300

    fixed problem with vpc_security_group_ids 	modified:   instances.tf

--- instances.tf
+++ instances.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "public-ec2" {
   ami = var.instance-ami
   instance_type = "t2.micro"
   subnet_id = aws_subnet.public-subnet.id
-  security_groups = [aws_security_group.allow-ssh.name]
+  vpc_security_group_ids  = [aws_security_group.allow-ssh.id]
 
   key_name = aws_key_pair.kp.key_name
 
@@ -18,7 +18,7 @@ resource "aws_instance" "private-ec2" {
   ami = var.instance-ami
   instance_type = "t2.micro"
   subnet_id  = aws_subnet.private-subnet.id
-  security_groups = [aws_security_group.allow-ssh.name]
+  vpc_security_group_ids  = [aws_security_group.allow-ssh.id]
 
    key_name = aws_key_pair.kp.key_name
 
