fe89409dbc6cf82b9f73dbff8d07c5b0e03ca191
belalelnady <belalelnadyz@gmail.com>
2024-09-04 16:14:15 +0300

    new file: s3.tf -ADD the backend s3 bucket with dynamoDB for state lock

--- main.tf
+++ main.tf
@@ -1,142 +1,142 @@
-# --------------------instances
+# # --------------------instances
 
-# resource "aws_instance" "public-ec2" {
-#   ami = "ami-0a0e5d9c7acc336f1"
-#   instance_type = "t2.micro"
-#   tags = {
-#     Name = "public-ec2"
+# # resource "aws_instance" "public-ec2" {
+# #   ami = "ami-0a0e5d9c7acc336f1"
+# #   instance_type = "t2.micro"
+# #   tags = {
+# #     Name = "public-ec2"
+# #   }
+# # }
+
+# # resource "aws_instance" "private-ec2" {
+# #   ami = "ami-0a0e5d9c7acc336f1"
+# #   instance_type = "t2.micro"
+# #   tags = {
+# #     Name = "private-ec2"
+# #   }
+# # }
+
+# # --------------------VPC
+
+# resource "aws_vpc" "terraform-lab-vpc" {
+#   cidr_block = "10.0.0.0/16"
+#     tags = {
+#     Name = "terraform-lab-vpc"
 #   }
 # }
 
-# resource "aws_instance" "private-ec2" {
-#   ami = "ami-0a0e5d9c7acc336f1"
-#   instance_type = "t2.micro"
+# # --------------------Subnet
+# resource "aws_subnet" "public-subnet" {
+#   vpc_id   = aws_vpc.terraform-lab-vpc.id
+#   cidr_block     = "10.0.1.0/24"
+#   availability_zone = "us-east-1a"
 #   tags = {
-#     Name = "private-ec2"
+#     Name = "public-subnet"
 #   }
 # }
 
-# --------------------VPC
+# resource "aws_subnet" "private-subnet" {
+#   vpc_id            =  aws_vpc.terraform-lab-vpc.id
+#   cidr_block        = "10.0.2.0/24"
+#   availability_zone = "us-east-1a"
+#   tags = {
+#     Name = "private-subnet"
+#   }
+# }
 
-resource "aws_vpc" "terraform-lab-vpc" {
-  cidr_block = "10.0.0.0/16"
-    tags = {
-    Name = "terraform-lab-vpc"
-  }
-}
+# # ------GATEWAY------
+# resource "aws_internet_gateway" "internetgw" {
+#   vpc_id = aws_vpc.terraform-lab-vpc.id
+#   tags = {
+#     Name = "Terraform-internetgw"
+#   }
+# }
 
-# --------------------Subnet
-resource "aws_subnet" "public-subnet" {
-  vpc_id   = aws_vpc.terraform-lab-vpc.id
-  cidr_block     = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
-  tags = {
-    Name = "public-subnet"
-  }
-}
+# # -------NAT
 
-resource "aws_subnet" "private-subnet" {
-  vpc_id            =  aws_vpc.terraform-lab-vpc.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1a"
-  tags = {
-    Name = "private-subnet"
-  }
-}
+# # we need an ip to use with the nat
+# resource "aws_eip" "nat-ip" {
+#  #Elastic IP should be allocated for use within a VPC 
+#   vpc = true
+# }
 
-# ------GATEWAY------
-resource "aws_internet_gateway" "internetgw" {
-  vpc_id = aws_vpc.terraform-lab-vpc.id
-  tags = {
-    Name = "Terraform-internetgw"
-  }
-}
-
-# -------NAT
-
-# we need an ip to use with the nat
-resource "aws_eip" "nat-ip" {
- #Elastic IP should be allocated for use within a VPC 
-  vpc = true
-}
-
-resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat-ip.id
-  subnet_id = aws_subnet.public-subnet.id
-  tags = {
-    Name = "nat-gateway"
-  }
-}
+# resource "aws_nat_gateway" "nat" {
+#   allocation_id = aws_eip.nat-ip.id
+#   subnet_id = aws_subnet.public-subnet.id
+#   tags = {
+#     Name = "nat-gateway"
+#   }
+# }
 
 
 
 
-# -------ROUTING TABLE------
+# # -------ROUTING TABLE------
 
-#public rout table
-resource "aws_route_table" "public" {
-   vpc_id = aws_vpc.terraform-lab-vpc.id
+# #public rout table
+# resource "aws_route_table" "public" {
+#    vpc_id = aws_vpc.terraform-lab-vpc.id
 
-  # connect to internet through the internet gw
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.internetgw.id
-  }
-  tags = {
-    Name = "public-route-table"
-  }
-}
+#   # connect to internet through the internet gw
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.internetgw.id
+#   }
+#   tags = {
+#     Name = "public-route-table"
+#   }
+# }
 
-#private rout table
-resource "aws_route_table" "private" {
-    vpc_id = aws_vpc.terraform-lab-vpc.id
+# #private rout table
+# resource "aws_route_table" "private" {
+#     vpc_id = aws_vpc.terraform-lab-vpc.id
 
-  # connect to internet through the nat
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat.id
-  }
-  tags = {
-    Name = "private-route-table"
-  }
-}
-
-
-
-# ------ to add the subnets to the route tables 
-resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public-subnet.id
-  route_table_id = aws_route_table.public.id
-}
-
-resource "aws_route_table_association" "private" {
-  subnet_id      = aws_subnet.private-subnet.id
-  route_table_id = aws_route_table.private.id
-}
+#   # connect to internet through the nat
+#   route {
+#     cidr_block     = "0.0.0.0/0"
+#     nat_gateway_id = aws_nat_gateway.nat.id
+#   }
+#   tags = {
+#     Name = "private-route-table"
+#   }
+# }
 
 
 
-# ---------------Security group
+# # ------ to add the subnets to the route tables 
+# resource "aws_route_table_association" "public" {
+#   subnet_id      = aws_subnet.public-subnet.id
+#   route_table_id = aws_route_table.public.id
+# }
 
-resource "aws_security_group" "allow-ssh" {
-  description = "Allow SSH"
-  vpc_id = aws_vpc.terraform-lab-vpc.id
+# resource "aws_route_table_association" "private" {
+#   subnet_id      = aws_subnet.private-subnet.id
+#   route_table_id = aws_route_table.private.id
+# }
 
 
-  ingress {
-    cidr_blocks = ["0.0.0.0/0"]
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-  }
-  egress {
-    cidr_blocks = ["0.0.0.0/0"]
-    from_port = 0
-    to_port = 0
-    # -1 means aplly all protocols tcp ssh udp  etc...
-    protocol = "-1"
-  }
-  tags = {
-    "Name" = "Terraform-SG"
-  }
-}
+
+# # ---------------Security group
+
+# resource "aws_security_group" "allow-ssh" {
+#   description = "Allow SSH"
+#   vpc_id = aws_vpc.terraform-lab-vpc.id
+
+
+#   ingress {
+#     cidr_blocks = ["0.0.0.0/0"]
+#     from_port = 22
+#     to_port = 22
+#     protocol = "tcp"
+#   }
+#   egress {
+#     cidr_blocks = ["0.0.0.0/0"]
+#     from_port = 0
+#     to_port = 0
+#     # -1 means aplly all protocols tcp ssh udp  etc...
+#     protocol = "-1"
+#   }
+#   tags = {
+#     "Name" = "Terraform-SG"
+#   }
+# }
---
+++ s3.tf
@@ -0,0 +1,41 @@
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-lab-bucket-sstate"
+
+}
+
+
+resource "aws_s3_bucket_versioning" "versioning" {
+    
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+
+resource "aws_dynamodb_table" "terraform-lock-state" {
+  name         = "terraform-lock-state"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name = "terraform-lock-state"
+  }
+}
+
+
+
+# terraform {
+#   backend "s3" {
+#     bucket         = "terraform-lab-bucket-sstate"
+#     key            = "dev/terraform.tfstate"
+#     region         = "us-east-1"
+#     dynamodb_table = "terraform-lock-state"
+#     encrypt        = true
+#   }
+# }
